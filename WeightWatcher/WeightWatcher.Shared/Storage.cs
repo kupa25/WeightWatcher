4e67746c0c1a58472ae18465ec5a01974daafa0d
kupa25@gmail.com <haran802>
2014-09-16 21:50:54 -0400

    forgot to add Storage file and packages.

---
+++ WeightWatcher/WeightWatcher.Shared/Storage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Windows.Storage;
+using Newtonsoft.Json;
+
+namespace WeightWatcher
+{
+    public class Storage
+    {
+        private static ApplicationDataContainer cloudStorage = ApplicationData.Current.RoamingSettings;
+
+        public static ObservableCollection<DailyWeight> GetValuesFromCloud()
+        {
+            var result = new ObservableCollection<DailyWeight>();
+
+            foreach (var item in cloudStorage.Values)
+            {
+                result.Add(JsonConvert.DeserializeObject<DailyWeight>(item.Value.ToString()));
+            }
+
+            return result;
+        }
+
+        public static void AddValues(string value)
+        {
+            cloudStorage.Values.Add(GetkeyValuepair(value));
+        }
+
+        private static KeyValuePair<string, object> GetkeyValuepair(string value)
+        {
+            return new KeyValuePair<string, object>(DateTime.Now.ToString(), JsonConvert.SerializeObject( new DailyWeight() { Date = DateTime.Now, Weight = double.Parse(value) }) );
+        }
+    }
+}
